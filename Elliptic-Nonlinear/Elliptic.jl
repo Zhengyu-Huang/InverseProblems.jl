75ab33417a9672cca1e1e6883d684a45cc558f69
Daniel Z. Huang <hzyhzy2014@gmail.com>
2020-10-18 20:40:51 -0700

    posterior distribution Elliptic

--- Elliptic-Nonlinear/Elliptic.jl
+++ Elliptic-Nonlinear/Elliptic.jl
@@ -2,6 +2,7 @@ using Random
 using Distributions
 using PyPlot
 using LinearAlgebra
+include("../Plot.jl")
 include("../RUKI.jl")
 include("../RExKI.jl")
 include("../REnKI.jl")
@@ -24,7 +25,7 @@ end
 
 function backward(p::Array{Float64,1}, args)
     x1, x2 = 0.25, 0.75
-
+    
     #
     #  x1  -x1^2/2 + x1/2      u2           = p1
     #  x2  -x2^2/2 + x2/2   exp(-u1)        = p2
@@ -32,12 +33,12 @@ function backward(p::Array{Float64,1}, args)
     #        u2           = q1
     #      exp(-u1)       = q2
     #    
-
+    
     X = [x1  -x1^2/2+x1/2; x2  -x2^2/2+x2/2]
-
+    
     q = X\p
     
-    return [-log(q[2]) ; q[1]]
+    return [-log(abs(q[2])) ; q[1]]
 end
 
 function ensemble(params_i::Array{Float64, 2})
@@ -92,7 +93,7 @@ end
 
 function UKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2}, 
     θ0_bar::Array{Float64,1}, θθ0_cov::Array{Float64,2}, 
-    α_reg::Float64, N_iter::Int64 = 100)
+    α_reg::Float64, N_iter::Int64, update_cov::Int64)
     
     
     parameter_names = ["u₁", "u₂"]
@@ -106,7 +107,6 @@ function UKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
     t_cov,
     α_reg)
     
-    
     for i in 1:N_iter
         
         update_ensemble!(ukiobj, ens_func) 
@@ -115,6 +115,11 @@ function UKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
         
         @info "g_bar = ", ukiobj.g_bar[i], "g_t = ", ukiobj.g_t
         @info "loss = ", (ukiobj.g_bar[i] - ukiobj.g_t)'/ukiobj.obs_cov*(ukiobj.g_bar[i] - ukiobj.g_t)
+        
+        
+        if (update_cov) > 0 && (i%update_cov == 0) 
+            ukiobj.θθ_cov[1] = copy(ukiobj.θθ_cov[end])
+        end
         
         
     end
@@ -126,7 +131,7 @@ end
 
 function ExKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2}, 
     θ0_bar::Array{Float64,1}, θθ0_cov::Array{Float64,2}, 
-    α_reg::Float64, N_iter::Int64 = 100)
+    α_reg::Float64, N_iter, update_cov::Int64)
     
     
     parameter_names = ["u₁", "u₂"]
@@ -140,7 +145,6 @@ function ExKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
     t_cov,
     α_reg)
     
-    
     for i in 1:N_iter
         
         update_ensemble!(ukiobj, ens_func) 
@@ -150,6 +154,10 @@ function ExKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
         @info "g_bar = ", ukiobj.g_bar[i], "g_t = ", ukiobj.g_t
         @info "loss = ", (ukiobj.g_bar[i] - ukiobj.g_t)'/ukiobj.obs_cov*(ukiobj.g_bar[i] - ukiobj.g_t)
         
+        
+        if (update_cov) > 0 && (i%update_cov == 0) 
+            ukiobj.θθ_cov[1] = copy(ukiobj.θθ_cov[end])
+        end
         
     end
     
@@ -164,7 +172,7 @@ function EnKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
     parameter_names = ["u₁", "u₂"]
     
     ens_func(θ_ens) = ensemble(θ_ens)
-
+    
     enkiobj = EnKIObj("ETKI",
     parameter_names,
     N_ens,
@@ -188,72 +196,65 @@ function EnKI(t_mean::Array{Float64,1}, t_cov::Array{Float64,2},
     return enkiobj
 end
 
-obs = [27.5; 79.7]
-obs_cov = [0.1^2   0.0; 0.0  0.1^2]
-μ0 = [0.0, 0.0] 
-cov_sqr0    = [1.0  0.0; 0.0 10.0]
-cov0 = cov_sqr0 * cov_sqr0 
 
 
+function Ellitic_Posterior_Plot()
 
-
-α_reg,  N_iter = 0.0, 2
-ukiobj = UKI(obs, obs_cov,  μ0, cov0 , α_reg,  N_iter)
-uki_θ_bar  = ukiobj.θ_bar[end]
-uki_θθ_cov = ukiobj.θθ_cov[end]
-@info uki_θ_bar, uki_θθ_cov
-det_θθ_cov = det(uki_θθ_cov)
-
-Nx = 100; Ny = 200
-xx = Array(LinRange(-5, -1, Nx))
-yy = Array(LinRange(103, 106, Ny))
-Z = zeros(Float64, Nx, Ny)
-
-for ix = 1:Nx
-    for iy = 1:Ny
-        temp = [xx[ix] - uki_θ_bar[1]; yy[iy] - uki_θ_bar[2]]
-        Z[ix, iy] = exp(-0.5*(temp'/uki_θθ_cov*temp)) / (2 * pi * sqrt(det_θθ_cov))
+    # prior and covariance
+    obs = [27.5; 79.7]
+    obs_cov = [0.1^2   0.0; 0.0  0.1^2]
+    μ0 = [0.0; 0.0] 
+    cov_sqr0    = [1.0  0.0; 0.0 10.0]
+    cov0 = cov_sqr0 * cov_sqr0 
+    
+    # compute posterior distribution by MCMC
+    f_density(u) = f_posterior(u, nothing, obs, obs_cov, μ0, cov0) 
+    step_length = 1.0
+    n_ite , n_burn_in= 1000000, 100000
+    us = RWMH(f_density, μ0, step_length, n_ite)
+    for update_cov in [0,5]
+        
+        
+        # compute posterior distribution the uki method 
+        α_reg,  N_iter = 1.0, 30
+        ukiobj = ExKI(obs, obs_cov,  μ0, cov0 , α_reg,  N_iter, update_cov)
+        
+        Nx = 100; Ny = 200
+        xx = Array(LinRange(-4, -2, Nx))
+        yy = Array(LinRange(103, 106, Ny))
+        X,Y = repeat(xx, 1, Ny), repeat(yy, 1, Nx)'
+        Z = zeros(Float64, Nx, Ny)
+        
+        
+        nrows, ncols = 2, 3
+        fig, ax = PyPlot.subplots(nrows=nrows, ncols=ncols, sharex=true, sharey=true, figsize=(18,12))
+        for irow = 1:nrows
+            for icol = 1:ncols
+                # plot UKI results 
+                ites = 5*((irow-1)*ncols + icol)
+                uki_θ_bar = ukiobj.θ_bar[ites]
+                uki_θθ_cov = ukiobj.θθ_cov[ites]
+                det_θθ_cov = det(uki_θθ_cov)
+                for ix = 1:Nx
+                    for iy = 1:Ny
+                        temp = [xx[ix] - uki_θ_bar[1]; yy[iy] - uki_θ_bar[2]]
+                        Z[ix, iy] = exp(-0.5*(temp'/uki_θθ_cov*temp)) / (2 * pi * sqrt(det_θθ_cov))
+                    end
+                end
+                ax[irow, icol].contour(X, Y, Z, 50)
+                
+                # plot MCMC results 
+                
+                everymarker = 1
+                ax[irow, icol].scatter(us[n_burn_in:everymarker:end, 1], us[n_burn_in:everymarker:end, 2], s = 1)
+            end
+        end
+        
+        fig.savefig("Elliptic_UKI_update_cov"*string(update_cov)*".png")
+        close("all")
     end
+    
 end
 
-X,Y = repeat(xx, 1, Ny), repeat(yy, 1, Nx)'
-pcolormesh(X, Y, Z)
-colorbar()
 
-
-
-α_reg,  N_ens,  N_iter = 0.0, 1000,  2
-enkiobj = EnKI(obs, obs_cov,  μ0, cov_sqr0 , α_reg, N_ens, N_iter)
-enki_θ  = enkiobj.θ[end]
-scatter(enki_θ[:, 1], enki_θ[:, 2], c= "r")
-
-
-# posterior distribution
-f_density(u) = f_posterior(u, nothing, obs, obs_cov, μ0, cov0) 
-step_length = 1.0
-n_ite , n_burn_in= 1000000, 100000
-us = RWMH(f_density, μ0, step_length, n_ite)
-everymarker = 100
-scatter(us[n_burn_in:everymarker:end, 1], us[n_burn_in:everymarker:end, 2], s = 1e-2)
-us_mean = mean(us[n_burn_in:end, :], dims=1)
-us_cov = cov(us[n_burn_in:end, :], dims=1)
-
-# inverse probability push 
-# n_ens = 10000
-# ps = rand(MvNormal(obs, obs_cov), n_ens)
-# us = zeros(Float64, n_ens, 2)
-# for i = 1:n_ens
-#     us[i, :] = backward(ps[:, i], nothing)
-# end
-# scatter(us[:, 1], us[:, 2])
-# us_mean = mean(us, dims=1)
-# us_cov = cov(us, dims=1)
-
-us_exact = backward(obs, nothing)
-dpdu = dforward(us_exact, nothing)
-dudp = inv(dpdu)
-#scatter(us_exact[1], us_exact[2])
-
-
-# uki
-#scatter(uki_θ_bar[1], uki_θ_bar[2], c="r")
+Ellitic_Posterior_Plot()
