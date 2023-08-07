e884c1e4842d36d6fb913481675da0b20440b3a2
Daniel-Huang <dzhuang@caltech.edu>
2023-08-08 07:10:42 +0800

    start to update GMKI

--- Inversion/GMGD.jl
+++ Inversion/GMGD.jl
@@ -30,54 +30,8 @@ mutable struct GMGDObj{FT<:AbstractFloat, IT<:Int}
     metric::String
     "the method to approximate the expectation in the gradient flow"
     expectation_method::String
-    "weights"
-    c_weights::Union{Array{FT, 1}, Array{FT, 2}}
-    mean_weights::Array{FT, 1}
-    cov_weights::Array{FT, 1}
-    "adaptively adjust these weights based cov, otherwise fix it"
-    adapt_α::Bool
-    "parameters for updating weights"
-    trunc_α::FT
 end
 
-function update_weights!(c_weights, mean_weights, cov_weights; 
-    unscented_transform = "modified-2n+1", adapt_α = false, trunc_α = -1.0, covs = nothing)
-
-    N_θ = length(c_weights)
-    N_ens = length(mean_weights)
-
-    κ = 0.0
-    β = 2.0
-    α = min(sqrt(4/(N_θ + κ)), 1.0)
-
-    if trunc_α > 0
-        if adapt_α && covs != nothing 
-            for im = 1:size(covs,1)
-                _, D, _ = svd(covs[im,:,:])
-                α = min(α, trunc_α/sqrt(D[1]))
-            end
-        else
-            α = trunc_α
-        end
-    end
-
-
-
-    λ = α^2*(N_θ + κ) - N_θ
-
-    c_weights[1:N_θ]     .=  sqrt(N_θ + λ)
-    mean_weights[1] = λ/(N_θ + λ)
-    mean_weights[2:N_ens] .= 1/(2(N_θ + λ))
-    cov_weights[1] = λ/(N_θ + λ) + 1 - α^2 + β
-    cov_weights[2:N_ens] .= 1/(2(N_θ + λ))
-
-    @assert(unscented_transform == "modified-2n+1" || unscented_transform == "original-2n+1")
-    if unscented_transform == "modified-2n+1"
-        mean_weights[1] = 1.0
-        mean_weights[2:N_ens] .= 0.0
-    end
-
-end
 
 
 """
@@ -90,37 +44,10 @@ function GMGDObj(metric::String,
                 θ0_mean::Array{FT, 2},
                 θθ0_cov::Union{Array{FT, 3}, Nothing},
                 expectation_method::String = "random-sampling",
-                N_ens::IT = 1,
-                adapt_α = false,
-                trunc_α = -1.0) where {FT<:AbstractFloat, IT<:Int}
+                N_ens::IT = 1) where {FT<:AbstractFloat, IT<:Int}
 
 
     N_θ = size(θ0_mean, 2)
-    
-    if expectation_method == "unscented_transform_original-2n+1" ||  expectation_method == "unscented_transform_modified-2n+1"
-
-        # ensemble size
-        N_ens = 2*N_θ+1
-
-        c_weights = zeros(FT, N_θ)
-        mean_weights = zeros(FT, N_ens)
-        cov_weights = zeros(FT, N_ens)
-
-
-        update_weights!(c_weights, mean_weights, cov_weights; 
-        unscented_transform = chop(expectation_method, head=20, tail=0), adapt_α = adapt_α, trunc_α = trunc_α,  covs=θθ0_cov)
-    
-
-    elseif expectation_method == "random_sampling" 
-        c_weights = zeros(FT, N_θ)
-        mean_weights = ones(FT, N_ens)/N_ens
-        cov_weights = ones(FT, N_ens)/(N_ens-1)
-
-    else
-
-        error("expectation_method: ", expectation_method, " is not recognized")
-    
-    end
 
     N_modes = length(θ0_w)
 
@@ -141,8 +68,7 @@ function GMGDObj(metric::String,
                   iter,
                   update_covariance,
                   metric,
-                  expectation_method,
-                  c_weights, mean_weights, cov_weights, adapt_α, trunc_α)
+                  expectation_method)
 
 end
 
@@ -164,23 +90,12 @@ function construct_ensemble(gmgd::GMGDObj{FT, IT}, x_means::Array{FT,2}, x_covs:
             xs[im, :, :] = ones(N_ens)*x_means[im, :]' + rand(Normal(0, 1), N_ens, N_x) * chol_xx_cov'
         end
 
-    elseif expectation_method == "unscented_transform_modified-2n+1" || expectation_method == "unscented_transform_original-2n+1"
-        @assert(N_ens == 2*N_x+1)
-        c_weights = gmgd.c_weights
+    elseif expectation_method == "unscented_transform"
+        @assert(N_ens == 1)
         xs = zeros(FT, N_modes, N_ens, N_x)
         
         for im = 1:N_modes
-            chol_xx_cov = cholesky(Hermitian(x_covs[im,:,:])).L
-            x_mean = x_means[im, :]
-            x = zeros(FT, N_ens, N_x)
-            x[1, :] = x_mean
-            for i = 1: N_x
-                x[i+1,     :] = x_mean + c_weights[i]*chol_xx_cov[:,i]
-                x[i+1+N_x, :] = x_mean - c_weights[i]*chol_xx_cov[:,i]
-
-                # @info "construct_ensemble x_mean, c_weights, chol_xx_cov, x = ", x_mean, c_weights[i], chol_xx_cov[:, i], x[i+1,:]
-            end
-            xs[im, :, :] .= x 
+            xs[im, :, :] = x_means[im, :]
         end
 
     else 
@@ -202,32 +117,13 @@ function construct_mean(gmgd::GMGDObj{FT, IT}, xs::Array{FT}) where {FT<:Abstrac
     sizes_x = size(xs)[3:end]
 
     x_means = zeros(FT, N_modes, sizes_x...)
-    mean_weights = gmgd.mean_weights
+    mean_weights = 1/gmgd.N_ens
     for im = 1:N_modes
-        x_means[im, repeat([:],ndims_x)...] = sum(mean_weights.*xs[im, repeat([:],ndims_x+1)...], dims=1)[1, repeat([:],ndims_x)...] 
+        x_means[im, repeat([:],ndims_x)...] = sum(mean_weights*xs[im, repeat([:],ndims_x+1)...], dims=1)[1, repeat([:],ndims_x)...] 
     end
     return x_means
 end
 
-# """
-# construct_cov xx_cov from ensemble x and mean x_mean
-# """
-# function construct_cov(gmgd::GMGDObj{FT, IT}, xs::Array{FT,3}, x_means::Array{FT , 2}) where {FT<:AbstractFloat, IT<:Int}
-#     N_modes, N_ens = gmgd.N_modes, gmgd.N_ens
-#     N_ens, N_x = gmgd.N_ens, size(x_means[1, :],1)
-    
-#     cov_weights = gmgd.cov_weights
-
-#     xx_covs = zeros(FT, N_modes, N_x, N_x)
-
-#     for im = 1:N_modes
-#         for i = 1: N_ens
-#             xx_covs[im, :, :] .+= cov_weights[i]*(xs[im, i, :] - x_means[im, :])*(xs[im, i, :] - x_means[im, :])'
-#         end
-#     end
-
-#     return xx_covs
-# end
 
 
 
@@ -291,14 +187,9 @@ function update_ensemble!(gmgd::GMGDObj{FT, IT}, func_logρ::Function, dt::FT) w
     logθ_w  = gmgd.logθ_w[end]
     θθ_cov  = gmgd.θθ_cov[end]
 
-    if gmgd.expectation_method == "unscented_transform_original-2n+1" ||  gmgd.expectation_method == "unscented_transform_modified-2n+1"
-        update_weights!(gmgd.c_weights, gmgd.mean_weights, gmgd.cov_weights; 
-            unscented_transform = chop(gmgd.expectation_method, head=20, tail=0), trunc_α = gmgd.trunc_α, covs=θθ_cov)
-    end
 
     ############ Generate sigma points
     θ_p = construct_ensemble(gmgd, θ_mean, θθ_cov)
-    # @info " θ_p = ", θ_p
     ###########  Entropy term
     logρ, ∇logρ, ∇²logρ = compute_logρ_gm(θ_p, exp.(logθ_w), θ_mean, θθ_cov)
     logρ_mean, ∇logρ_mean, ∇²logρ_mean  = construct_mean(gmgd, logρ), construct_mean(gmgd, ∇logρ), construct_mean(gmgd, ∇²logρ)
@@ -315,6 +206,7 @@ function update_ensemble!(gmgd::GMGDObj{FT, IT}, func_logρ::Function, dt::FT) w
         for im = 1:N_modes
             θ_mean_n[im, :]    =  θ_mean[im, :] - dt*θθ_cov[im, :, :]*(∇logρ_mean[im, :] + ∇V_mean[im, :]) 
 
+            
             if update_covariance
                 θθ_cov_n[im, :, :] =  inv( inv(θθ_cov[im, :, :]) + dt*(∇²logρ_mean[im, :, :] + ∇²V_mean[im, :, :]) )
             else
@@ -366,10 +258,8 @@ function GMGD_Run(
     metric::String,
     update_covariance::Bool, 
     θ0_w::Array{FT, 1}, θ0_mean::Array{FT, 2}, θθ0_cov::Array{FT, 3},
-    expectation_method::String = "unscented_transform_modified-2n+1",
-    N_ens::IT = 1,
-    adapt_α = false,
-    trunc_α = -1.0) where {FT<:AbstractFloat, IT<:Int}
+    expectation_method::String = "unscented_transform",
+    N_ens::IT = 1) where {FT<:AbstractFloat, IT<:Int}
     
     
     
@@ -377,7 +267,7 @@ function GMGD_Run(
     metric, 
     update_covariance, 
     θ0_w, θ0_mean, θθ0_cov,
-    expectation_method, N_ens, adapt_α, trunc_α)
+    expectation_method, N_ens)
      
     func_logρ(θ_ens) = ensemble(θ_ens, forward)  
     
@@ -396,109 +286,3 @@ end
 
 
 
-######################### TEST #######################################
-
-
-# function gaussain_V(θ::Array{FT, 1}) where {FT<:AbstractFloat}
-#     # sample ρ ∝ exp(-θ'Aθ/2)
-#     N_θ = length(θ)
-#     A = Diagonal(ones(N_θ))
-#     V   = θ'*(A*θ)/2
-#     ∇V  = A*θ
-#     ∇²V = A
-#     return V, ∇V, ∇²V
-# end
-
-
-# T = 40000.0
-# N_iter = 40000 
-# N_modes = 2
-# N_θ = 1
-# θ0_w  = fill(1.0, N_modes)/N_modes
-# θ0_mean, θθ0_cov  = zeros(N_modes, N_θ), zeros(N_modes, N_θ, N_θ)
-# σ_0 = 0.1
-# Random.seed!(111);
-# for m = 1:N_modes
-#     θ0_mean[m, :]    .= rand(Normal(0, σ_0), N_θ)
-#     θθ0_cov[m, :, :] .= Array(Diagonal(fill(σ_0^2, N_θ)))
-# end
-# @info "Run GMKI with ", N_modes, " θ0_mean = ", θ0_mean
-# metric = "Fisher-Rao"
-# update_covariance = true
-# expectation_method = "unscented_transform_modified-2n+1"
-# N_ens = 2N_θ+1
-# # expectation_method = "random_sampling"
-# # N_ens = 2
-# gmgdobj = GMGD_Run(
-#     gaussain_V, 
-#     N_iter,
-#     T,
-#     metric,
-#     update_covariance, 
-#     θ0_w, θ0_mean, θθ0_cov,
-#     expectation_method,
-#     N_ens)
-
-
-   
-
-# Nx = 1000
-# θ_min, θ_max = -5.0, 5.0
-
-# # Plot reference
-# xx_ref = Array(LinRange(θ_min, θ_max, 1000))
-# yy_ref = copy(xx_ref)
-# for i = 1:length(xx_ref)
-#         V, _, _ = gaussain_V(xx_ref[i:i])
-#         yy_ref[i] = exp(-V[1])
-# end
-# yy_ref .= yy_ref / ( sum(yy_ref)*(xx_ref[2] - xx_ref[1]) )
-
-# using PyPlot
-# include("Plot.jl")
-
-# fig, ax = PyPlot.subplots(sharex=false, sharey="row", figsize=(6,6))
-# ax.plot(xx_ref, yy_ref, "-s", label="Reference", color="grey", linewidth=2, fillstyle="none", markevery=25)
-# iter = N_iter
-# Nx = 1000
-# xxs, zzs = zeros(N_modes, Nx), zeros(N_modes, Nx)
-# θ_min = minimum(gmgdobj.θ_mean[iter][:,1] .- 5sqrt.(gmgdobj.θθ_cov[iter][:,1,1]))
-# θ_max = maximum(gmgdobj.θ_mean[iter][:,1] .+ 5sqrt.(gmgdobj.θθ_cov[iter][:,1,1]))            
-
-# for i =1:N_modes
-#     xxs[i, :], zzs[i, :] = Gaussian_1d(gmgdobj.θ_mean[iter][i,1], gmgdobj.θθ_cov[iter][i,1,1], Nx, θ_min, θ_max)
-#     zzs[i, :] *= exp(gmgdobj.logθ_w[iter][i])
-#     ax.plot(fill(gmgdobj.θ_mean[iter][i,1], 11), LinRange(0,1,11), color = "C"*string(i), marker="o", fillstyle="none", markevery=5)
-#     ax.plot(xxs[1,:], zzs[i, :], linestyle=":", color = "C"*string(i), fillstyle="none", markevery=100, linewidth=2)
-# end
-# ax.plot(xxs[1,:], sum(zzs, dims=1)', linestyle="-", fillstyle="none", markevery=100, label="GMGD", linewidth=2)
-# ax.legend()
-
-
-
-# fig, ax = PyPlot.subplots(sharex=false, sharey="row", figsize=(6,6))
-# θ_w = exp.(hcat(gmgdobj.logθ_w...))
-# θ_mean = hcat(gmgdobj.θ_mean...)
-# for i =1:N_modes
-#     ax.plot(Array(1:N_iter), θ_w[i, 1:N_iter], "--", color = "C"*string(i), fillstyle="none", markevery=5, label = (i == 1 ? "weight" : nothing) )
-#     ax.plot(Array(1:N_iter), θ_mean[i, 1:N_iter], ":", color = "C"*string(i), fillstyle="none", markevery=5, label= (i == 1 ? "mean" : nothing))
-# end
-# ax.legend()
-
-# θ_w = exp.(hcat(gmgdobj.logθ_w...))
-# for i =1:N_modes
-# ax[2, N_modes].plot(Array(1:N_iter), θ_w[i, 1:N_iter], marker=linestyles[i], color = "C"*string(i), fillstyle="none", markevery=5, label= "mode "*string(i))
-# end
-# ax[2, N_modes].legend()
-# ax[2, N_modes].set_xlabel("Iterations")
-# end
-# ax[1, 1].set_ylabel("Densities")
-# ax[2, 1].set_ylabel("Weights")
-
-# fig.tight_layout()
-# fig.savefig("1D-density-"*string(σ_η)*"-"*string(iter)*".pdf")
-
-
-
-
-# end
